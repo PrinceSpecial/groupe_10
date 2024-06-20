991789fe4395ddf58b8287579b9abf788d1d3a11
Delprogram <fidelahouanhou19@gmail.com>
2024-06-20 20:56:28 +0200

    Update numpy.py
    Implémentation des listes à 1d et 2d.

--- task_1/numpy.py
+++ task_1/numpy.py
@@ -1,5 +1,11 @@
+from typing import Union, List
 class Array:
+    def __init__(self, data: Union[List[int], List[List[int]]]):
+        self.data = data
+        for i in data:
+            if i == list:
+                self.shape = (len(data), len(data[0]))
+            else:
+                self.shape = (1, len(data))
 
-    # Constructor
-    def __init__(self, data):
-        pass
+    def 
