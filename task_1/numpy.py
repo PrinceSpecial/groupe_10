d1e13f877dff42bdbcc0a54f7e1ce30ffe8fe26d
Delprogram <fidelahouanhou19@gmail.com>
2024-06-21 11:25:56 +0200

    Méthode __add__

--- task_1/numpy.py
+++ task_1/numpy.py
@@ -8,4 +8,13 @@ class Array:
             else:
                 self.shape = (1, len(data))
 
-    def 
+    def     def __add__(self, other: "Array"):
+        print(len(other.data))
+        if self.shape == other.shape:
+            return [other.data[i] + self.data[i] for i in range(len(other.data)) if other.data == List[int]]
+
+
+b = Array([1, 2, 3, 4])
+a = Array([1, 2, 3, 4])
+c = a + b
+print(c)
